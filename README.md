86a252ce22f530f45d21946519174f25c90a532f
kmagritte <k.kshr@yandex.ru>
2022-10-17 13:49:38 +0300

    Added eda_config and starting eda now from the command line, you can pass the data path and the path to save the report

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This project uses [Heart Disease Cleveland UCI](https://www.kaggle.com/datasets/
 This package allows you to train model for determine the heart disease.
 1. Clone this repository to your machine.
 2. Download [Heart Disease Cleveland UCI](https://www.kaggle.com/datasets/cherngs/heart-disease-cleveland-uci) dataset, save csv locally (default path is *data* in repository's root).
+3. If you want to get an EDA report with pandas profiling, the command should look like this. The generated report will be placed in *reports*.
+```sh
+python ml_project/eda.py configs/eda_config.yaml
+```
+There is also an EDA.ipynb report in *notebooks*.
--- ml_project/eda.py
+++ ml_project/eda.py
@@ -1,14 +1,17 @@
-import os
 import pandas as pd
 from pandas_profiling import ProfileReport
+from enities.eda_params import read_eda_params
+import click
 
 
-def eda_pandas_profiling() -> None:
-    path_with_data = "data/heart_cleveland_upload.csv"
-    path_to_save = "reports"
-    dataset = pd.read_csv(path_with_data)
+@click.command()
+@click.argument("config_path")
+def eda_pandas_profiling(config_path: str) -> None:
+    eda_params = read_eda_params(config_path)
+    dataset = pd.read_csv(eda_params.path_with_data)
     profile = ProfileReport(dataset, title="Pandas Profiling Report", explorative=True)
-    profile.to_file(os.path.join(path_to_save, "EDA.html"))
+    profile.to_file(eda_params.path_to_save)
 
 
-eda_pandas_profiling()
+if __name__ == "__main__":
+    eda_pandas_profiling()
