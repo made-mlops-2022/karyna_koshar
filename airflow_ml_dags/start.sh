6c0833c07d1a9148d5f212a137f62805652bc3e5
kmagritte <k.kshr@yandex.ru>
2022-11-20 22:19:37 +0300

    Added start.sh

---
+++ airflow_ml_dags/start.sh
@@ -0,0 +1,5 @@
+export MLRUNS_PATH=$(pwd)/mlruns
+export DATA_PATH=$(pwd)/data
+export FERNET_KEY=$(python3 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
+
+docker compose up --build
